3f44619acb2c3e65d0d36fb1c919fd8ccffe54db
doanle84 <doan.le@anatas.com>
2017-06-20 15:28:17 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,8 +5,18 @@ url = "http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demo
 #
 # # Read in a page
 html = scraperwiki.scrape(url)
-print html
+#print html
 
+root = lxml.html.fromstring(html)
+for tr in root.cssselect("div[align='left'] tr"):
+    tds = tr.cssselect("td")
+    print tds
+    if len(tds)==12:
+        data = {
+            'country' : tds[0].text_content(),
+            'years_in_school' : int(tds[4].text_content())
+        }
+        print data
 #
 # # Find something on the page using css selectors
 # root = lxml.html.fromstring(html)
